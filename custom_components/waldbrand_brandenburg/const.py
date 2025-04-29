d8811ef1efb7e40e6b676672956c4121d9863cea
bitsbeauty <bitsbeauty@users.noreply.github.com>
2025-04-29 17:32:34 +0200

    Update const.py

--- custom_components/waldbrand_brandenburg/const.py
+++ custom_components/waldbrand_brandenburg/const.py
@@ -1,27 +1,49 @@
 DOMAIN = "waldbrand_brandenburg"
 PLATFORM = "sensor"
 UPDATE_INTERVAL = 86400
-URL = "https://mluk.brandenburg.de/sixcms/media.php/9/Waldbrandgefahrenstufen_BB.cod"
+
+URL = "https://mluk.brandenburg.de/mluk/de/wgs.xml"
 
 CONF_COUNTY = "county"
 
-COUNTY_LIST = {
-    "prignitz": 0,
-    "ostprignitz_ruppin": 1,
-    "oberhavel": 2,
-    "uckermark": 3,
-    "barnim": 4,
-    "maerkisch_oderland": 5,
-    "oder_spree": 6,
-    "spree_neisse": 7,
-    "oberspreewald_lausitz": 8,
-    "elbe_elster": 9,
-    "teltow_flaeming": 10,
-    "potsdam_mittelmark": 11,
-    "havelland": 12,
-    "dahme_spreewald": 13,
-    "frankfurt_oder": 14,
-    "cottbus": 15,
-    "brandenburg_an_der_havel": 16,
-    "potsdam": 17
+COUNTY_CHOICES = {
+    "barnim": "Barnim",
+    "brandenburg_an_der_havel": "Brandenburg an der Havel",
+    "cottbus": "Cottbus",
+    "dahme_spreewald": "Dahme-Spreewald",
+    "elbe_elster": "Elbe-Elster",
+    "frankfurt_oder": "Frankfurt (Oder)",
+    "havelland": "Havelland",
+    "maerkisch_oderland": "Märkisch-Oderland",
+    "oberhavel": "Oberhavel",
+    "oberspreewald_lausitz": "Oberspreewald-Lausitz",
+    "oder_spree": "Oder-Spree",
+    "ostprignitz_ruppin": "Ostprignitz-Ruppin",
+    "potsdam": "Potsdam",
+    "potsdam_mittelmark": "Potsdam-Mittelmark",
+    "prignitz": "Prignitz",
+    "spree_neisse": "Spree-Neiße",
+    "teltow_flaeming": "Teltow-Fläming",
+    "uckermark": "Uckermark"
 }
+
+COUNTY_ORDER = [
+    "prignitz",
+    "ostprignitz_ruppin",
+    "oberhavel",
+    "uckermark",
+    "barnim",
+    "maerkisch_oderland",
+    "oder_spree",
+    "spree_neisse",
+    "oberspreewald_lausitz",
+    "elbe_elster",
+    "teltow_flaeming",
+    "potsdam_mittelmark",
+    "havelland",
+    "dahme_spreewald",
+    "frankfurt_oder",
+    "cottbus",
+    "brandenburg_an_der_havel",
+    "potsdam",
+]
